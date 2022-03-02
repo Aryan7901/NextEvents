d4a01fae8a295414691c17bc5856fa337351fb31
Aryan7901 <85547188+Aryan7901@users.noreply.github.com>
2022-03-02 19:49:20 +0530

    changed Link button

--- components/ui/LinkBtn.jsx
+++ components/ui/LinkBtn.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Button } from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/router";
 function LinkBtn(props) {
-  const router = useRouter();
   if (!!props.link) {
     return (
-      <Button
-        className={props.className}
-        variant="contained"
-        sx={{ color: "white" }}
-        onClick={() => router.push(props.link)}
-      >
-        {props.text}
-      </Button>
+      <Link href={props.link} passHref>
+        <Button
+          className={props.className}
+          variant="contained"
+          sx={{ color: "white" }}
+        >
+          {props.text}
+        </Button>
+      </Link>
     );
   } else {
     return (
